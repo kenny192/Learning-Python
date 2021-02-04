b10ba00424237e88c974b0c5eed246c51494b86a
kenny192 <78522764+kenny192@users.noreply.github.com>
2021-02-04 15:19:31 +0800

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,11 @@
+#codin:utf-8
+"""
+calculat your age after 10 years
+"""
+name = input("your name: ")
+age = int(input("your age: "))
+
+after = age + 10
+
+print("your name is ", name)
+print("after 10 years, you are ", after, "years old.")
